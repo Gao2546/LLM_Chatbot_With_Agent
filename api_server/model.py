db9529272d6b7b4144498b01b4366970e21f5e5a
Gao2546 <athipyuthaworawit@gmail.com>
2025-12-25 09:28:01 +0700

    update requirement docker and api server

--- api_server/model.py
+++ api_server/model.py
@@ -1740,4 +1740,4 @@ if __name__ == '__main__':
     #             n_class=n_class
 
     #         )
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
